be7dd1e9d7abe0ad98a4bc890b13cb0afe81e209
Jarne <jarne@users.noreply.github.com>
2020-06-14 16:02:01 +0200

    Improved special character list, updated PHP testing version

--- src/jarne/password/utils/Characters.php
+++ src/jarne/password/utils/Characters.php
@@ -8,7 +8,7 @@ namespace jarne\password\utils;
 class Characters {
     const LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const NUMBERS = "1234567890";
-    const SPECIAL_CHARACTERS = "!?@(){}[]\/=~$%&#*-+.,_";
+    const SPECIAL_CHARACTERS = "!?@()[]/=$%&#*-+.,;_";
 
     const VOWEL_LETTERS = "aeiouAEIOU";
     const OTHER_LETTERS = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
